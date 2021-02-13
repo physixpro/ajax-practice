f78f98f9ebc35408198c47f2eccedc60beb2b03f
Keagan <esorts.k990@gmail.com>
2021-02-13 01:05:14 -0500

    practied get with params and post requests

--- Test2.js
+++ Test2.js
@@ -1,21 +1,43 @@
 import React,{useState,} from 'react'
+import axios from "axios"
 
 const Test2 = () => {
 
-    const[users,setUsers] = useState([])
+    const [users,setUsers] = useState([])
 
     useEffect(() => {
         const fetchUsers = async () => {
-            const res = axios.get("http://localhost:3001/feed")
+            const res = await axios.get("http://localhost:3001/feed")
             setUsers(res.data)
         }
         fetchUsers()
     },[])
-    return (
+
+    return(
         <div>
-            
+            {
+                users.map((user => (
+                    <div key={user._id}>
+                        <p>{user.status}</p>
+                    </div>
+                )))
+            }
         </div>
     )
+    // const[users,setUsers] = useState([])
+
+    // useEffect(() => {
+    //     const fetchUsers = async () => {
+    //         const res = axios.get("http://localhost:3001/feed")
+    //         setUsers(res.data)
+    //     }
+    //     fetchUsers()
+    // },[])
+    // return (
+    //     <div>
+            
+    //     </div>
+    // )
 }
 
 export default Test2
--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 // app.use(express.urlencoded({ extended: true}))
 
 // mongoose.connect("mongodb+srv://data:1234@cluster0.qvhok.mongodb.net/Facebook?retryWrites=true&w=majority", {useUnifiedTopology:true, useNewUrlParser:true})
-// const db = mongoose.connection 
+// const db = mongoose.connection
 
 // db.on("error", console.error.bind(console, "connection error"))
 // db.once("open", function callback() {
@@ -19,12 +19,6 @@
 //     const feed = await db.collection("feed").find({}).toArray();
 //     res.json(feed)
 // })
-
-
-
-
-
-
 
 // const port = process.env.PORT || 3001
 // app.listen(port, () => console.log(`Server is running on port ${port}...`))
@@ -41,7 +35,7 @@
 // app.use(express.urlencoded({extended:true}))
 
 // mongoose.connect("mongodb+srv://data:1234@cluster0.qvhok.mongodb.net/Facebook?retryWrites=true&w=majority",{useUnifiedTopology:true, useNewUrlParser:true})
-// const db = mongoose.connection 
+// const db = mongoose.connection
 
 // db.on("error", console.error.bind(console, "connection error"))
 // db.once("open", function callback () {
@@ -53,31 +47,79 @@
 //     res.json(feed)
 // })
 
-
 // const port = process.env.PORT = 3001
 // app.listen(port, () => console.log(`server is runnign on port ${port}...`))
 
-const express = require("express")
-const mongoose = require("mongoose")
-const app = express();
+// const express = require("express")
+// const mongoose = require("mongoose")
+// const app = express();
+// const cors = require("cors");
+// const ObjectID = require("mongodb").ObjectID;
+// app.use(cors())
+// app.use(express.urlencoded({extended:true}))
+// app.use(express.json())
+
+// mongoose.connect("mongodb+srv://data:1234@cluster0.qvhok.mongodb.net/Facebook?retryWrites=true&w=majority", {useUnifiedTopology:true, useNewUrlParser:true})
+// const db = mongoose.connection
+
+// db.on("error", console.error.bind(console,"connection error"))
+// db.once("open", function callback() {
+//     console.log("database is up and running")
+// })
+
+// app.get('/feed', async(req,res) => {
+//     const feed = await db.collection("feed").find({}).toArray();
+//     res.json(feed)
+// })
+
+// const port = process.env.PORT || 3001;
+// app.listen(port, () => console.log(`server is running on port ${port}`))
+
+const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const app = express();
+app.use(express());
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 const ObjectID = require("mongodb").ObjectID;
-app.use(cors())
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
 
-mongoose.connect("mongodb+srv://data:1234@cluster0.qvhok.mongodb.net/Facebook?retryWrites=true&w=majority", {useUnifiedTopology:true, useNewUrlParser:true})
-const db = mongoose.connection
+mongoose.connect(
+  "mongodb+srv://data:1234@cluster0.qvhok.mongodb.net/Facebook?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
+const db = mongoose.connection;
 
-db.on("error", console.error.bind(console,"connection error"))
+const name = "Keagan"
+db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function callback() {
-    console.log("database is up and running")
-})
+  console.log(`Good evening ${name}, the database is up and running`);
+});
 
-app.get('/feed', async(req,res) => {
-    const feed = await db.collection("feed").find({}).toArray();
+app.get('/feed', async (req,res) => {
+    const feed = await db.collection("feed").find({}).toArray()
     res.json(feed)
 })
 
+app.get('/feed/:status', async(req,res) => {
+    const status = req.params.status;
+    const user = await db.collection("feed").find({status:status}).toArray();
+    res.json(user)
+})
+
+app.delete('/feed/:userId', async(req,res) => {
+    const id = req.params.userId
+    const x = await db.collection("feed").deleteOne({_id: new ObjectID (id)})
+    res.json("user deleted")
+})
+
+app.post('/feed', async(req,res) => {
+    const message = req.body
+    const x = await db.collection("feed").insertOne(message)
+    console.log(message)
+    res.json("message added")
+})
+
 const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}...`));
